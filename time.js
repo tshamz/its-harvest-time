914ff4ea9cfd1d5f8617f8b643771bd6c4556ea8
Tyler Shambora <tyler@bvaccel.com>
2017-02-08 23:32:38 -0800

    wip

--- time.js
+++ time.js
@@ -6,8 +6,6 @@ var express           = require('express');
 var bodyParser        = require('body-parser');
 var methodOverride    = require('method-override');
 
-var config            = require('./config.json');
-
 /**
  * Harvest Integration
  */

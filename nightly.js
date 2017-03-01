43e58c6cc1ce0f15ea2ff2af5f8c01826b466ff5
Tyler Shambora <tyler@bvaccel.com>
2017-03-01 14:38:38 -0800

    fix bugs

--- nightly.js
+++ nightly.js
@@ -5,6 +5,10 @@ const Q = require('q');
 const harvest = require('./harvest/harvest.js');
 const mongo = require('./database/database.js');
 
+const startMongo = function () {
+  return mongo.start();
+};
+
 harvest.poll()
 .done(function (totals) {
   mongo.write({ document: totals, collection: 'time'})
